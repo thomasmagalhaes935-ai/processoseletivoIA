7d1c511627e5d1d5ef236de37da7ec2d457d5632
thomasmagalhaes935-ai <thomasmagalhaes935@gmail.com>
2026-04-24 08:33:40 +0000

    Implementação do optimize_model

--- optimize_model.py
+++ optimize_model.py
@@ -1,4 +1,32 @@
 import tensorflow as tf
 import os
 
-#insira seu código aqui
+def main():
+    print("Carregando modelo treinado (Keras)...\n")
+
+    model = tf.keras.models.load_model("model.h5", compile=False)
+
+    print("Convertendo modelo para TensorFlow Lite...\n")
+
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+
+    # Otimização (Dynamic Range Quantization)
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+
+    tflite_model = converter.convert()
+
+    with open("model.tflite", "wb") as f:
+        f.write(tflite_model)
+
+    print("Modelo convertido e salvo como model.tflite")
+
+    # Comparação de tamanho
+    original = os.path.getsize("model.h5") / 1024
+    otimizado = os.path.getsize("model.tflite") / 1024
+
+    print("\nCOMPARAÇÃO:")
+    print(f"Tamanho original: {original:.2f} KB")
+    print(f"Tamanho otimizado: {otimizado:.2f} KB")
+
+if __name__ == "__main__":
+    main()
